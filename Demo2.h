1858985b4a12bc01473ba4f4a5a57a1482cd6948
gary <gary@garydeMacBook-Pro.local>
2016-04-25 15:03:06 +0800

    first

---
+++ Demo2.h
@@ -0,0 +1,23 @@
+//
+//  __  __          ____           _          _
+//  \ \/ / /\_/\   /___ \  _   _  (_)   ___  | | __
+//   \  /  \_ _/  //  / / | | | | | |  / __| | |/ /
+//   /  \   / \  / \_/ /  | |_| | | | | (__  |   <
+//  /_/\_\  \_/  \___,_\   \__,_| |_|  \___| |_|\_\
+//
+//  Created by gary on 16/4/25.
+//  Copyright © 2016年 gary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Demo2: UITableViewController
+#pragma mark- as
+
+#pragma mark- model
+
+#pragma mark- view
+
+#pragma mark- api
+
+@end
---
+++ Demo2.m
@@ -0,0 +1,59 @@
+//
+//  __  __          ____           _          _
+//  \ \/ / /\_/\   /___ \  _   _  (_)   ___  | | __
+//   \  /  \_ _/  //  / / | | | | | |  / __| | |/ /
+//   /  \   / \  / \_/ /  | |_| | | | | (__  |   <
+//  /_/\_\  \_/  \___,_\   \__,_| |_|  \___| |_|\_\
+//
+//  Created by gary on 16/4/25.
+//  Copyright © 2016年 gary. All rights reserved.
+//
+
+#import "Demo2.h"
+
+@interface Demo2 ()
+
+@end
+
+@implementation Demo2
+
+#pragma mark - def
+
+#pragma mark - override
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+// 创建页面内控件的地方。
+- (void)uxy_createViews
+{
+    
+}
+
+// 创建页面内控件事件的地方
+- (void)uxy_createEvents
+{
+    
+}
+
+// 如果页面加载过程需要读取数据, 则写在这个地方。
+- (void)uxy_loadData
+{
+    
+}
+#pragma mark - api
+
+#pragma mark - model event
+#pragma mark 1 notification
+#pragma mark 2 KVO
+
+#pragma mark - view event
+#pragma mark 1 target-action
+#pragma mark 2 delegate dataSource protocol
+
+#pragma mark - private
+#pragma mark - getter / setter
+
+#pragma mark -
+
+@end
